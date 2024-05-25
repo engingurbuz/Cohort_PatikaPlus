91bf92bdc216b4ec3d47ada4b7392bf57aaad69d
Engin GÜRBÜZ <engurbuz@gmail.com>
2024-05-25 18:48:08 +0300

    week1 teknik içerik 3

--- src/week1/teknik_icerik_3/TersÜçgen.java
+++ src/week1/teknik_icerik_3/TersÜçgen.java
@@ -2,19 +2,23 @@ package week1.teknik_icerik_3;
 
 import java.util.Scanner;
 
-public class TersÜçgen {
+import java.util.Scanner;
+
+public class TersÜçgen { // Sınıf ismi Türkçe olarak daha açıklayıcı
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner tarayici = new Scanner(System.in); // Daha Türkçe bir isim kullanın
 
-        System.out.print("Basamak Sayısı Giriniz: ");
-        int basamakSayisi = scanner.nextInt();
+        System.out.print("Satır Sayısı Giriniz (Üçgenin Yüksekliği): "); // Açıklayıcı bir istem kullanın
+        int satirSayisi = tarayici.nextInt(); // Daha açıklayıcı bir değişken ismi kullanın
 
-        for (int i = basamakSayisi; i >= 1; i--) {
+        // Dış döngü, en üstten (satirSayisi) başlayarak ve azalarak her satırda gezinir
+        for (int i = satirSayisi; i >= 1; i--) {
+            // İç döngü, sütunlardan geçerek, geçerli satırdaki her sütun için yıldız basar
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("*"); // Bir yıldız yazdırın
             }
-            System.out.println();
+            System.out.println(); // Bir satır için yıldızları yazdırdıktan sonra sonraki satıra geç
         }
     }
 }
